d4537e16c3707279df8cbbd63c54372ca41ff35b
Advaitva <guptaadvaitva@gmail.com>
2020-05-21 23:33:55 +0530

    models user profiles posts

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = Users = mongoose.model("users", UserSchema);
