1c83926ca166df9b57a563a736d57cdd53352462
Advaitva <guptaadvaitva@gmail.com>
2020-06-24 15:02:52 +0530

    added states in login and register

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -2,36 +2,61 @@ import React, { Component } from "react";
 import "./styles/login-register.css";
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {},
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+  onSubmit(event) {
+    event.preventDefault();
+    const user = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+    console.log(user);
+  }
   render() {
     return (
       <div>
-        <div class="body">
+        <div className="body">
           <h1>Log In</h1>
-          <div class="form">
-            <div class="input-Div">
+          <div className="form">
+            <div className="input-Div">
               <input
                 type="text"
                 name="email"
                 id="email"
-                autocomplete="off"
+                autoComplete="off"
+                value={this.state.email}
+                onChange={this.onChange}
                 required
               />
-              <label class="label-name" for="email">
-                <span class="label-span">E-mail</span>
+              <label className="label-name" htmlFor="email">
+                <span className="label-span">E-mail</span>
               </label>
             </div>
 
-            <div class="input-Div">
+            <div className="input-Div">
               <input
                 type="password"
                 name="password"
                 id="pass"
-                autocomplete="off"
+                autoComplete="off"
+                value={this.state.password}
+                onChange={this.onChange}
                 required
               />
 
-              <label class="label-name" for="password">
-                <span class="label-span">Password</span>
+              <label className="label-name" htmlFor="password">
+                <span className="label-span">Password</span>
               </label>
             </div>
 
--- client/src/components/Auth/Register.js
+++ client/src/components/Auth/Register.js
@@ -2,34 +2,63 @@ import React, { Component } from "react";
 import "./styles/login-register.css";
 
 class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      password2: "",
+      errors: {},
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  onChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+  onSubmit(event) {
+    event.preventDefault();
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password: this.state.password2,
+    };
+    console.log(newUser);
+  }
   render() {
     return (
       <div>
-        <div class="body">
+        <div className="body">
           <h1>Sign Up</h1>
-          <div class="form">
-            <div class="input-Div">
+          <div className="form">
+            <div className="input-Div">
               <input
                 type="text"
                 name="name"
                 id="name"
-                autocomplete="off"
+                autoComplete="off"
+                value={this.state.name}
+                onChange={this.onChange}
                 required
               />
-              <label class="label-name" for="name">
-                <span class="label-span">Name</span>
+              <label className="label-name" htmlFor="name">
+                <span className="label-span">Name</span>
               </label>
             </div>
-            <div class="input-Div">
+            <div className="input-Div">
               <input
                 type="text"
                 name="email"
                 id="email"
-                autocomplete="off"
+                autoComplete="off"
+                value={this.state.email}
+                onChange={this.onChange}
                 required
               />
-              <label class="label-name" for="email">
-                <span class="label-span">E-mail</span>
+              <label className="label-name" htmlFor="email">
+                <span className="label-span">E-mail</span>
               </label>
             </div>
             <span id="mailMessage">
@@ -37,33 +66,37 @@ class Register extends Component {
               profile image will be used.)
             </span>
 
-            <div class="input-Div">
+            <div className="input-Div">
               <input
                 type="password"
                 name="password"
                 id="pass"
-                autocomplete="off"
+                autoComplete="off"
+                value={this.state.password}
+                onChange={this.onChange}
                 required
               />
 
-              <label class="label-name" for="password">
-                <span class="label-span">Password</span>
+              <label className="label-name" htmlFor="password">
+                <span className="label-span">Password</span>
               </label>
             </div>
-            <div class="input-Div">
+            <div className="input-Div">
               <input
                 type="password"
                 name="password2"
                 id="pass2"
-                autocomplete="off"
+                autoComplete="off"
+                value={this.state.password2}
+                onChange={this.onChange}
                 required
               />
-              <label class="label-name" for="password2">
-                <span class="label-span">Confirm Password</span>
+              <label className="label-name" htmlFor="password2">
+                <span className="label-span">Confirm Password</span>
               </label>
             </div>
             <div>
-              <button type="submit" id="submit">
+              <button type="submit" id="submit" onClick={this.onSubmit}>
                 Sign Up
               </button>
             </div>
