28eb2cc3f8681190256e4dfaad5ea3f1f7ecb9d9
Advaitva <advaitva.gupta@outlook.com>
2021-05-12 22:40:55 +0530

    honeybadger setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import Honeybadger from "@honeybadger-io/js";
+import ErrorBoundary from "@honeybadger-io/react";
 
+Honeybadger.configure({
+  apiKey: "99603a69",
+  environment: "production",
+});
+window.onerror((e) => {
+  console.log("Error: " + e);
+});
 ReactDOM.render(
-  <React.StrictMode>
+  <ErrorBoundary honeybadger={Honeybadger}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ErrorBoundary>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
